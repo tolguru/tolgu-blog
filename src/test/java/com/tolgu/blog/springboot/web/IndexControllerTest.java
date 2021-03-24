b4688ba7d62afe1ccca4e3f902d40bc28e459b3b
rnjsehdgks01 <62535103+rnjsehdgks01@users.noreply.github.com>
2021-03-24 18:16:08 +0900

    로그인 페이지 초안, Access, SecurityConfig 커스텀 로그인 페이지로 접속 변경, index 페이지 접근 권한 변경, 에러 수정

--- src/test/java/com/tolgu/blog/springboot/web/IndexControllerTest.java
+++ src/test/java/com/tolgu/blog/springboot/web/IndexControllerTest.java
@@ -1,27 +1,33 @@
 package com.tolgu.blog.springboot.web;
 
+import com.tolgu.blog.springboot.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(controllers = AccessController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class) })
 public class IndexControllerTest {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
-    public void 메인페이지_로딩() {
-        //when
-        String body = this.restTemplate.getForObject("/", String.class);
+    public void 메인페이지_로딩() throws Exception {
 
-        //then
-        assertThat(body).contains("Spring");
+        mvc.perform(get("/"))
+                .andExpect(status().isOk());
     }
 }
