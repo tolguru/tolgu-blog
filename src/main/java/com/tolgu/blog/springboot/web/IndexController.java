b260f94e9a7a22ae3cc0e3e99dd1f1b20c79ca40
rnjsehdgks01 <62535103+rnjsehdgks01@users.noreply.github.com>
2021-03-25 02:15:35 +0900

    IndexController, Session 롤백

--- src/main/java/com/tolgu/blog/springboot/web/IndexController.java
+++ src/main/java/com/tolgu/blog/springboot/web/IndexController.java
@@ -17,9 +17,12 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
